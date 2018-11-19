e5c85821d5c4880c90d314f77f5bc3afe0b69fb7
bjudob <konczbotondvagyok@gmail.com>
2018-11-19 18:31:51 +0200

    abc index bug fixed

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'id2643544_training_db');
+   if (!defined('DB_SERVER')) define('DB_SERVER', 'localhost');
+   if (!defined('DB_USERNAME')) define('DB_USERNAME', 'root');
+   if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
+   if (!defined('DB_DATABASE')) define('DB_DATABASE', 'id2643544_training_db');
    $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
    mysqli_query($con,"SET NAMES utf8");
    mysqli_set_charset($con,"UTF8");
